07d4efd32e3147a3e54a62fe25fceb024d5a28da
moniker001 <lim.tejong@gmail.com>
2015-04-08 15:33:10 -0500

    added more commands

--- bash/rcchelp.sh
+++ bash/rcchelp.sh
@@ -17,7 +17,35 @@
 
 rcchelp () {
   case $1 in
-    (user) phldap uid=$2 ;;
-    # etc.
+    # rccsoftware
+    software)
+      echo "More information can be found in the online RCC documentation at https://rcc.uchicago.edu/docs/software/modules/index.html"
+      module display $2 ;;
+    
+    # rccaccount
+    accounts)
+      if [ "$1" == "allocations" ]
+        accounts allocations ;;
+      elif [ "$1" == "balance" ]
+        accounts balance ;;
+      else [ "$1" == "usage" ]
+        accounts usage ;;
+    group-members)
+      # TODO
+    project-quota)
+      project-quota $3 ;;
+    quota)
+      # TODO
+    restore)
+      # TODO
+    user)
+      phldap uid=$2 ;;
+      # TODO - how to print groups
+    # rccscheduler
+    
+    # rcchelp all
+    
+    # rcchelp search
+    
   esac
 }
