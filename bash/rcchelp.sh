9879cbf473a415c08e2d90395cae60bbdb5598d2
moniker001 <lim.tejong@gmail.com>
2015-04-22 15:31:04 -0500

    finished qos and reorganized code

--- bash/rcchelp.sh
+++ bash/rcchelp.sh
@@ -16,152 +16,200 @@
 #
 # -------------------------------------------------------------------
 
-#$LOGFILE = "/rcchelp.log"
+printHeader () {
+  clear
+  echo "-------------------------------------------------------------------------------"
+  echo " *      The University of Chicago Research Computing Center Help System      * "
+  echo "-------------------------------------------------------------------------------"
+}
 
-rcchelp () {
+printFooter () {
+  echo "-------------------------------------------------------------------------------"
+  echo " *      Please contact help@rcc.uchicago.edu with any questions              * "
+  echo "-------------------------------------------------------------------------------"
+}
+
+# Displays list of all rcchelp commands
+printAll () {
+  echo "        All commands are preceded by \"rcchelp\"                               "
+  echo "        Some commands may only work on Midway, such as \"quota\"               "
+  echo "-------------------------------------------------------------------------------"
+  echo "        COMMAND              DESCRIPTION                                       "
+  echo
+  echo "        all                  Displays list of all rcchelp commands             "
+  echo "                             \"rcchelp all\"                                   "
+  echo
+  echo "        allocations          Displays your allocations of SUs                  "
+  echo "                             \"rcchelp allocations\"                           "
+  echo
+  echo "        balance              Displays your remaining balance of SUs            "
+  echo "                             \"rcchelp balance\"                               "
+  echo
+  echo "        group-members        Displays all members in specified group           "
+  echo "                             \"rcchelp group-members [RCC group name]\"        "
+  echo
+  echo "        project-quota        Displays quota information for project folder     "
+  echo "                             \"rcchelp project-quota [project directory]\"     "
+  echo
+  echo "        qos                  Displays information on Slurm QoS                 "
+  echo "                             \"rcchelp qos\"                                   "
+  echo
+  echo "        quota                Displays your file system usage and limits        "
+  echo "                             \"rcchelp user [cnet id]\"                        "
+  echo
+  echo "        restore              Restores a file to a given folder or (by default) "
+  echo "                             the file's original location based on a snapshot  "
+  echo "                             \"rcchelp restore [file] [[destination]]\"        "
+  echo
+  echo "        sinfo                Displays information on available Slurm queues    "
+  echo "                             \"rcchelp sinfo [[options]]\"                     "
+  echo
+  echo "        usage                Displays your consumption of SUs                  "
+  echo "                             \"rcchelp usage\"                                 "
+  echo
+  echo "        user                 Displays detailed infomation on specified user    "
+  echo "                             \"rcchelp user [cnet id]\"                        "
+}
+
+# Displays your allocations of SUs
+printAllocations () {
+  accounts allocations
+}
+
+# Displays your remaining balance of SUs 
+printBalance () {
+  accounts balance
+}
+
+# Displays information about specified software
+printSoftware () {
+  echo "        More information can be found in the online RCC documentation          "
+  echo "        https://rcc.uchicago.edu/docs/software/modules/index.html              "
+  module display $1
+}
+
+# Displays all members in specified group
+printGroup () {
+  # TODO - formatting
+  getent group $1
+}
+
+printPQuota () {
+  project-quota $1
+}
+
+# Displays your file system usage and limits
+printQuota () {
+  quota $1
+}
+
+# Displays your consumption of SUs
+printUsage () {
+  accounts usage
+}
+
+# Displays detailed infomation on specified user
+printUser () {
+  # TODO - formatting
+  phldap uid=$1
+  groups $1
+}
+
+# Restores a file to a given folder or (by default) the file's original location based on a snapshot
+printRestore () {
+  #TODO
+  restore $1 $2
+}
+
+# Displays information on available Slurm queues
+printSinfo () {
+  echo "        See online Slurm documentation for more information on \"sinfo\"       "
+  echo "        https://computing.llnl.gov/linux/slurm/sinfo.html                      "
+  echo "-------------------------------------------------------------------------------"
   case "$1" in
-    # Displays information about specified software
-    software)
-      echo "-------------------------------------------------------------------"
-      echo
-      echo "  More information can be found in the online RCC documentation    "
-      echo "  https://rcc.uchicago.edu/docs/software/modules/index.html        "
-      echo
-      module display $2
-      ;;
-
-    # Displays your allocations of SUs
-    allocations)
-      accounts allocations
-      ;;
-    
-    # Displays your remaining balance of SUs 
-    balance)
-      accounts balance
-      ;;
-    
-    # Displays your consumption of SUs
-    usage)
-      accounts usage
-      ;;
-    
-    # Displays all members in specified group  
-    group-members)
-      getent group $2
-      ;;
-    
-    # Displays quota information for project folder
-    project-quota)
-      project-quota $2
-      ;;
-    
-    # Displays your file system usage and limits
-    quota)
-      quota $2
-      ;;
-    
-    # Restores a file to a given folder or (by default) the file's original location based on a snapshot
-    restore)
-      restore $2 $3
-      ;;
-    
-    # Displays detailed infomation on specified user
-    user)
-      clear
-      phldap uid=$2
-      groups $2
-      ;;
-    
-    # Displays information on Slurm QoS
-    qos)
-      # TODO
-      echo TODO
-      ;;
-    
-    # Displays information on available Slurm queues
-    sinfo)
-      # TODO
-      clear
-      echo '-------------------------------------------------------------------------------'
-      echo ' *      The University of Chicago Research Computing Center Help System      * '
-      echo '-------------------------------------------------------------------------------'
-      echo '        See online Slurm documentation for more information on "sinfo"         '
-      echo '        https://computing.llnl.gov/linux/slurm/sinfo.html                      '
-      echo '-------------------------------------------------------------------------------'
-      case "$2" in
-      # condensed display
-      -s)
-        sinfo -o "%9P %6D %3c %7g %28f %22N"
-        ;;
-      # verbose display
-      -v)
-        sinfo --long
-        # TODO - add more sinfo options
-        ;;
-      # default is condensed
-      *)
-        sinfo -o "%9P %6D %3c %7g %28f %22N"
-        ;;
-      esac
-      ;;
-    
-    # Displays list of all rcchelp commands
-    all)
-      clear
-      echo '-------------------------------------------------------------------------------'
-      echo ' *      The University of Chicago Research Computing Center Help System      * '
-      echo '-------------------------------------------------------------------------------'
-      echo '        All commands are preceded by "rcchelp"                                 '
-      echo '        Some commands may only work on Midway, such as "quota"                 '
-      echo '-------------------------------------------------------------------------------'
-      echo '        COMMAND              DESCRIPTION                                       '
-      echo
-      echo '        all                  Displays list of all rcchelp commands             '
-      echo '                             "rcchelp all"                                     '
-      echo
-      echo '        allocations          Displays your allocations of SUs                  '
-      echo '                             "rcchelp allocations"                             '
-      echo
-      echo '        balance              Displays your remaining balance of SUs            '
-      echo '                             "rcchelp balance"                                 '
-      echo
-      echo '        group-members        Displays all members in specified group           '
-      echo '                             "rcchelp group-members [RCC group name]"          '
-      echo
-      echo '        project-quota        Displays quota information for project folder     '
-      echo '                             "rcchelp project-quota [project directory]"       '
-      echo
-      echo '        qos                  Displays information on Slurm QoS                 '
-      echo '                             "rcchelp qos"                                     '
-      echo
-      echo '        quota                Displays your file system usage and limits        '
-      echo '                             "rcchelp user [cnet id]"                          '
-      echo
-      echo '        restore              Restores a file to a given folder or (by default) '
-      echo "                             the file's original location based on a snapshot  "
-      echo '                             "rcchelp restore [file] [[destination]]"          '
-      echo
-      echo '        sinfo                Displays information on available Slurm queues    '
-      echo '                             "rcchelp sinfo [[options]]"                         '
-      echo
-      echo '        usage                Displays your consumption of SUs                  '
-      echo '                             "rcchelp usage"                                   '
-      echo
-      echo '        user                 Displays detailed infomation on specified user    '
-      echo '                             "rcchelp user [cnet id]"                          '
-      echo '-------------------------------------------------------------------------------'
+  -v)
+    # verbose display
+    sinfo --long
+    # TODO - add more sinfo options
     ;;
-    
-    *)
-      echo "Error: \"rcchelp $1 $2\" is not a valid command"
-      echo "See \"rcchelp all\" for list of commands"
-      ;;
-      
+  *)
+    # default display
+    sinfo -o "%9P %R %6D %3c %7g %28f %22N"
+    ;;
   esac
 }
 
-# Call function with parameters
+# Displays information on Slurm QoS
+printQos () {
+  echo "        See online Slurm documentation for more information                    "
+  echo "        https://computing.llnl.gov/linux/slurm/sacctmgr.html                   "
+  echo "        http://slurm.schedmd.com/qos.html                                      "
+  echo "-------------------------------------------------------------------------------"
+  case "$1" in
+    -v)
+     # verbose display
+      sacctmgr show qos
+      ;;
+    *)
+      # default display
+      sacctmgr show qos format=name,MaxNodes,MaxCPUs,MaxCPUsPerUser,MaxNodesPerUser,MaxJobsPerUser%7,MaxSubmitJobsPerUser%9,MaxWallDurationPerJob
+      echo "-------------------------------------------------------------------------------"
+      echo " *      QOS Table Definitions                                                * "
+      echo "-------------------------------------------------------------------------------"
+      echo "        COLUMN             DESCRIPTION                                         "
+      echo
+      echo "        MaxNodes           Maximum number of nodes that each job can use       "
+      echo
+      echo "        MaxCPUs            Maximum number of CPUs that each job can use        "
+      echo
+      echo "        MaxCPUsPU          Maximum total number of CPUs each user can use      "
+      echo
+      echo "        MaxNodesPU         Maximum total number of nodes each user can use     "
+      echo
+      echo "        MaxJobs            Maximum number of jobs each user can have running   "
+      echo
+      echo "        MaxSubmit          Maximum number of jobs each user can have submitted "
+      echo
+      echo "        MaxWall            Maximum wall clock time each job can use            "
+      echo "                           (days-hours:minutes:seconds)                        "
+      ;;
+  esac
+}
+
+rcchelp () {
+  printHeader
+  case "$1" in
+    software)
+      printSoftware $2 ;;
+    allocations)
+      printAllocations ;;
+    balance)
+      printBalance ;;
+    usage)
+      printUsage ;;
+    group-members)
+      printGroup $2 ;;
+    project-quota)
+      printPQuota $2 ;;
+    quota)
+      printQuota $2 ;;
+    restore)
+      printRestore $2 $3 ;;
+    user)
+      printUser $2 ;;
+    qos)
+      printQos $2 ;;
+    sinfo)
+      printSinfo $2 ;;
+    all)
+      printAll ;;
+    *)
+      # Display list of rcchelp commands for bad commands
+      printAll ;;
+  esac
+  printFooter
+}
+
+# Call function with input parameters
 rcchelp $1 $2
-
-
 
